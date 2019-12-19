ad9c85dacb914405abc95b29299b8d50a51e486a
renqingguo <renqingguo@rqg>
2019-12-19 10:48:27 +0800

    头条PC-登录模块-数据绑定及校验

--- src/views/login.vue
+++ src/views/login.vue
@@ -4,16 +4,19 @@
         <div class="login_d">
             <img src="../assets/img/logo_index.png" alt="">
         </div>
-        <el-form>
-            <el-form-item>
-                <el-input placeholder="请输入手机号"></el-input>
+        <el-form :model="loginForm" :rules="loginRules" ref="myForm">
+            <el-form-item prop="mobile">
+                <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
             </el-form-item>
-            <el-form-item>
-                <el-input style="width:70%;" placeholder="请输入验证码"></el-input>
+            <el-form-item  prop="code">
+                <el-input v-model="loginForm.code" style="width:70%;" placeholder="请输入验证码" ></el-input>
                 <el-button plain class="anniu">验证码</el-button>
             </el-form-item>
+            <el-form-item prop="check">
+                <el-checkbox v-model="loginForm.check">我已阅读条款</el-checkbox>
+            </el-form-item>
             <el-form-item>
-                <el-button type="primary" style="width:100%">登录</el-button>
+                <el-button type="primary" style="width:100%" @click="loginSubmit">登录</el-button>
             </el-form-item>
         </el-form>
     </el-card>
@@ -21,7 +24,46 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入你的手机号' },
+          { pattern: /^1[3456789]\d{9}$/, message: '请输入合法手机号' }],
+
+        code: [
+          { required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '验证码为6位数' }],
+
+        check: [{ validator (rule, value, callback) {
+          // 判断规则是否正确
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('请同意条款'))
+          }
+        } }]
+      }
+    }
+  },
+  methods: {
+    loginSubmit () {
+      this.$refs.myForm.validate(function (isOk) {
+        if (isOk) {
+          // 校验通过 调用接口检查数据
+          console.log('通过')
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang = 'less' scoped>

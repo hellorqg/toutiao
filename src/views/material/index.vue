6f98587e76ac1c7f9b6b29d1044af2ce95981519
renqingguo <renqingguo@rqg>
2019-12-24 09:50:25 +0800

    头条PC-素材列表-素材的收藏和删除

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,24 +1,28 @@
 <template>
-  <el-card v-loading='loading'>
+  <el-card v-loading="loading">
     <breadcrumb slot="header">
       <template slot="title">素材管理</template>
     </breadcrumb>
     <el-row type="flex" justify="end">
       <!-- 上传图片 -->
-      <el-upload action="" :http-request="upImg" :show-file-list='false' >
+      <el-upload action :http-request="upImg" :show-file-list="false">
         <el-button type="primary" size="small">上传图片</el-button>
       </el-upload>
     </el-row>
     <!-- tab栏切换 -->
-    <el-tabs v-model="activeName" @tab-click='getCollet'>
+    <el-tabs v-model="activeName" @tab-click="getCollet">
       <!-- 全部图片 -->
       <el-tab-pane label="全部" name="all">
         <div class="allImg">
           <el-card v-for="item in imgs" :key="item.id" class="listImg">
             <img :src="item.url" alt style="height:100%" />
-            <el-row class="icon" type="flex" justify="space-around"  align='middle'>
-              <i class="el-icon-star-on"></i>
-              <i class="el-icon-delete-solid"></i>
+            <el-row class="icon" type="flex" justify="space-around" align="middle">
+              <i
+                @click="collects(item)"
+                class="el-icon-star-on"
+                :style="{color: item.is_collected ? 'red' : '#000'}"
+              ></i>
+              <i @click="delImg(item)" class="el-icon-delete-solid"></i>
             </el-row>
           </el-card>
         </div>
@@ -26,7 +30,7 @@
 
       <!-- 收藏图片 -->
       <el-tab-pane label="收藏" name="collect">
-         <div class="allImg">
+        <div class="allImg">
           <el-card v-for="item in imgs" :key="item.id" class="listImg">
             <img :src="item.url" alt style="height:100%" />
           </el-card>
@@ -42,11 +46,37 @@ export default {
     return {
       activeName: 'all', // 默认选中的tab栏
       imgs: [], // 用户图片的列表
-      collect: true, // 用户图片是否收藏 布尔值
+      // collect: true, // 用户图片是否收藏 布尔值
       loading: false // 图片上传时加载
     }
   },
   methods: {
+    // 删除图片
+    delImg (item) {
+      this.$confirm('确定删除？').then(() => {
+        this.$axios({
+          url: `/user/images/${item.id}`,
+          method: 'delete'
+        }).then(res => {
+          // console.log(res)
+          this.getandShow()
+        })
+      }
+
+      )
+    },
+    // 取消/收藏图片
+    collects (item) {
+      this.$axios({
+        url: `/user/images/${item.id}`,
+        method: 'put',
+        data: {
+          collect: !item.is_collected
+        }
+      }).then(res => {
+        this.getandShow()
+      })
+    },
     // 上传图片
     upImg (params) {
       this.loading = true
@@ -108,6 +138,9 @@ export default {
       bottom: 0;
       font-size: 23px;
       background-color: #f4f5f6;
+    }
+    i {
+      cursor: pointer;
     }
   }
 }
